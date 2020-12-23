98c02527603207832dbc7608955681715460aa24
Adeyinka Giwa <adeyinka36@gmail.com>
2020-12-23 12:33:00 +0000

    first project first draft

---
+++ src/Book/Book.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Book = props =>{
+
+
+    return (
+        <li>
+        <div className="book" >
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${props.data.imageLinks.smallThumbnail})`}}></div>
+          <div className="book-shelf-changer">
+        
+            <select  value={props.search?props.search:props.data.shelf} onChange={(e)=>{e.preventDefault();props.changeShelf(props.data,e)}}>
+              <option  value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          
+          </div>
+        </div>
+        <div className="book-title">{props.data.title}</div>
+        <div className="book-authors">{props.data.authors}</div>
+      </div>
+      </li>
+    )
+}
+
+
